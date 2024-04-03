080be32363a2b1d107df720b84faee7c223a8e6e
Felix Hummel <felix@felixhummel.de>
2024-04-03 13:53:50 +0200

    add felix-markdown-*

--- felix-markdown-files-as-h1.py
+++ felix-markdown-files-as-h1.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
+
+# pip install linkify-it-py mistletoe structlog
 import sys
 
 from pathlib import Path
@@ -15,13 +17,46 @@ def _replacements():
     pairs = zip(range(6,0,-1), range(7,1,-1))
     for a, b in pairs:
         yield f'<h{a}', f'<h{b}'
+        yield f'</h{a}', f'</h{b}'
 
 # log.info(list(_replacements()))
 
+print("""
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>md</title>
+    <link rel="stylesheet" type="text/css" href="https://www.felixhummel.de/css/bamboo.css">
+    <style type="text/css" media="screen">
+      .felix-md h1 {
+        font-size: 1rem;
+        border-left: 1em solid;
+        padding-left: 0.3em;
+      }
+    </style>
+</head>
+<body class="felix-md">
+""".strip())
+
 files = [Path(x) for x in sys.argv[1:]]
+
+print("""
+<h1>TOC</h1>
+<ol>
+""".strip())
+for f in files:
+    print(f'<li><a href="#{f.name}">{f.name}</a></li>')
+print("</ol>")
+
 for f in files:
     rendered = mistletoe.markdown(f.read_text())
     for a, b in _replacements():
         rendered = rendered.replace(a, b)
-    rendered = f'<h1>{f.name}</h1>\n{rendered}\n\n'
+    rendered = f'<h1 id="{f.name}">{f.name}</h1>\n{rendered}\n\n'
     print(rendered)
+print("""
+    </ol>
+</body>
+</html>
+""".strip())
